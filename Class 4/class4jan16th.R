76c642c5006ab5b45bd268cb54bc68e4d26aea3c
Zainab Fatima <zfatima@ucsd.edu>
2025-02-25 11:54:50 -0800

    class 4

---
+++ Class 4/class4jan16th.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "BIMM 143 Class 4 Rscript to PDF"
+#' author: Zainab Fatima
+#' ---
+
+
+
+x <- 1:50
+plot (x, sin(x))
+
+#Make this nicer
+plot(x, sin(x), col ="blue", type ="l", lwd=3)
+
